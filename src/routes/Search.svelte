05adc9ee99450fb277716530a985313a1e36d0e1
Maslin <maslinfar@gmail.com>
2023-03-05 17:38:48 -0800

    Added autofocus on search box, upgraded pkgs

--- src/routes/Search.svelte
+++ src/routes/Search.svelte
@@ -33,7 +33,7 @@
 
 <svelte:window on:keydown={handleKeyDown}/>
 <div class="searchBox">
-    <input bind:value={query} type="text" placeholder={provider} name="" id= "">
+    <input bind:value={query} type="text" placeholder={provider} autofocus>
 </div>
 <style>
     input{
@@ -44,7 +44,7 @@
         background: none;
         width: auto;
         outline: 0;
-        color: rgb(44 44 44 / 84%);
+        color: rgba(44, 44, 44, 0.84);
         border: 0px;
     }
     input::placeholder {
@@ -52,6 +52,6 @@
         font-weight: 400;
         text-align: center;
         background: none;
-        color: rgba(54, 54, 54, 0.678);
+        color: rgb(44, 44, 44, 0.84);
     }
 </style>
